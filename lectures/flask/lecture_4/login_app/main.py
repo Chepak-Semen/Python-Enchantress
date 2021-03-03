ccce54d0ca88c93bf91bcb3284e1d22f1ce71b27
Chepak_Semen <chepak.semmy@gmail.com>
2021-03-03 20:10:01 +0200

    10hw

--- lectures/flask/lecture_4/login_app/main.py
+++ lectures/flask/lecture_4/login_app/main.py
@@ -1,19 +1,17 @@
-from flask_login import login_required, current_user
 from flask import Blueprint, render_template
-
-from . import db
-
+from flask_login import login_required, current_user
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    response = {'success'}
+    return response, 200
 
 
 @main.route('/profile')
 @login_required
 def profile():
-
-    return render_template('profile.html', name=current_user.name)
+    response = {f'success, name is {current_user.name}'}
+    return response, 200
