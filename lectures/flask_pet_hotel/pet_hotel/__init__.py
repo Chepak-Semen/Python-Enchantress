c0576621c9d9b6f927b8205402061d9331c2c3e6
Chepak_Semen <chepak.semmy@gmail.com>
2021-03-19 18:56:17 +0200

    done

--- lectures/flask_pet_hotel/pet_hotel/__init__.py
+++ lectures/flask_pet_hotel/pet_hotel/__init__.py
@@ -1,8 +1,23 @@
 from flask import Flask
-from pet_hotel.routes import check_in_bp
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
+from .middleware import OurMiddleware
+
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(check_in_bp)
+    api = Api(app)
+    app.wsgi_app = OurMiddleware(app.wsgi_app)
+    app.config['SECRET_KEY'] = 'key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+
+    db.init_app(app=app)
+
+    from .routes import ShowActivities, SetDeletePet
+    api.add_resource(ShowActivities, '/show_activ')
+    api.add_resource(SetDeletePet, '/set_pet')
+
     return app
--- lectures/flask_pet_hotel/pet_hotel/routes.py
+++ lectures/flask_pet_hotel/pet_hotel/routes.py
@@ -1,10 +1,64 @@
-from flask import Blueprint, request
+from datetime import datetime, date
+
+from flask import request
+from flask_restful import Resource
+
+from models import Owner, Pet, Activities
+from . import db
 
 
-check_in_bp = Blueprint('chek-in', __name__, url_prefix='/check-in')
+class SetDeletePet(Resource):
+
+    @staticmethod
+    def post():
+        response = request.get_json()
+        db.session.add(Owner(name=response["name"],
+                             phone_number=response["phone_number"]
+                             ))
+        added_user = Owner.query.filter_by(name=response["name"],
+                                           phone_number=response["phone_number"]
+                                           ).first()
+        db.session.commit()
+        db.session.add(Pet(name=response["pet"]['name'],
+                           owner_id=added_user.id,
+                           room_id=response['pet']["room_id"],
+                           date=datetime.now(),
+                           ))
+        db.session.commit()
+        added_pet = Pet.query.filter_by(owner_id=added_user.id).first()
+        db.session.add(Activities(type=response['pet']["activity"]["activity_type"],
+                                  time=response['pet']["activity"]["time"],
+                                  pet_id=added_pet.id))
+        db.session.commit()
+        return {"status": "success"}, 201
+
+    @staticmethod
+    def delete():
+        response = request.get_json()
+        pet = Pet.query.filter_by(name=response["pet_name"], room_id=response["room_id"]).first()
+        owner_pet = Pet.query.filter_by(owner_id=pet.owner.id).all()
+        pet_activities = Activities.query.filter_by(pet_id=pet.id).all()
+
+        db.session.delete(pet)
+        db.session.delete(pet_activities)
+        db.session.commit()
+
+        if not owner_pet:
+            db.session.delete(Owner.query.filter_by(id=pet.owner.id).delete())
+            db.session.commit()
+
+        return (date.today() - pet.date).days, 200
 
 
-@check_in_bp.route('/', methods=['POST'])
-@check_in_bp.route('', methods=['POST'])
-def check_in():
-    return 'success'
+class ShowActivities(Resource):
+
+    @staticmethod
+    def post():
+        actv = []
+        response = request.get_json()
+        pet = Pet.query.filter_by(name=response["pet_name"]).first()
+        activities = Activities.query.filter_by(id=pet.id).all()
+        for i in activities:
+            actv.append({pet.name: {'type': i.type, 'hour': i.time}})
+        return actv
+
